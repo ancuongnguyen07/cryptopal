95b1aa439dc65bd093753b612c3c4c20a9748310
Cuong Nguyen <ancuongnguyen07@gmail.com>
2024-04-11 13:14:43 +0300

    [set1] challenge 8 done

---
+++ set1/S1_C8.go
@@ -0,0 +1,65 @@
+package set1
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"os"
+)
+
+func isECBEncrypted(ciphertext []byte) (bool, error) {
+	blockSize := 16
+
+	for i := 0; i < len(ciphertext); i += blockSize {
+		endIndex := min(i+blockSize, len(ciphertext))
+		firstCipherBlock := ciphertext[i:endIndex]
+		for k := i + blockSize; k < len(ciphertext); k += blockSize {
+			endIndex := min(k+blockSize, len(ciphertext))
+			secondCipherBlock := ciphertext[k:endIndex]
+			if bytes.Equal(firstCipherBlock, secondCipherBlock) {
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
+
+}
+
+func S1C8RunChallenge() {
+	file, err := os.Open("set1/8.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	ECBEncrypted := []int{}
+	var inputData [][]byte
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		hexCiphertext := scanner.Bytes()
+
+		ciphertext := make([]byte, hex.DecodedLen(len(hexCiphertext)))
+		n, err := hex.Decode(ciphertext, hexCiphertext)
+		if err != nil {
+			panic(err)
+		}
+		ciphertext = ciphertext[:n]
+
+		inputData = append(inputData, ciphertext)
+	}
+
+	for i, line := range inputData {
+		isECBEncryptedC, err := isECBEncrypted(line)
+		if err != nil {
+			panic(err)
+		}
+		if isECBEncryptedC {
+			ECBEncrypted = append(ECBEncrypted, i)
+		}
+	}
+
+	fmt.Println("Hex-encoded ciphertext that was ECB-encrypted (line number):\n", ECBEncrypted)
+
+}
